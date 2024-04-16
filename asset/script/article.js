709faf948f3f744816d90da9e13e340b91f18643
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-04-16 15:19:16 +0700

    Added table of content highlight

--- asset/script/article.js
+++ asset/script/article.js
@@ -1,15 +1,30 @@
 // script to fill table of content <aside> using headings from <article>
 (() => {
-    let list = document.createElement('ol');
+    let list = document.createElement('ol'),
+        headings = document.querySelectorAll('section > article > h2');
 
-    document.querySelectorAll('section > article > h2')
-    .forEach((heading, key) => {
-        heading.setAttribute('id', key)
+    headings.forEach((heading, key) => {
+        // set heading id
+        heading.setAttribute('id', key);
+
+        // add to table of content
         let li = document.createElement('li');
         li.innerHTML =
             `<a href="#${key}">${heading.innerHTML}</a>`;
         list.append(li);
     });
-
     document.querySelector('section > div > aside').append(list)
+
+    // scroll detection to highlight table of content
+    document.addEventListener('scroll', () => {
+        [...list.children].forEach(anchor => anchor.classList.remove('active'))
+
+        for (let l1 = 0; l1 < headings.length; l1--) {
+            let loc = headings[l1].getBoundingClientRect();
+            if (loc.top + loc.height > 0) {
+                list.children[l1].classList.add('active');
+                break
+            }
+        }
+    })
 })();
