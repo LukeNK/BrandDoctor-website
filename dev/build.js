28c07cbc87a819535255c1a9748742ca06d6e96c
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-05-09 15:14:56 +0700

    Build script to build posts

--- dev/build.js
+++ dev/build.js
@@ -23,6 +23,8 @@ module.exports = {
      * @param {Object} config The config file
      */
     onTranslationBuild: (dom, item, config) => {
+        console.warn('onTranslationBuild() is currently being skipped for migration')
+        return
         // add lazy loading to images
         dom.querySelectorAll('img').forEach(elm => {
             // if the image does not specified loading attr before hand
@@ -89,6 +91,7 @@ function buildPosts(folder, config) {
         postList[index] = data
     });
 
+
     postList.forEach(post => {
         // save to build
         let build = `/${folder}/${post.path.split('.')[0]}`;
@@ -106,31 +109,7 @@ function buildPosts(folder, config) {
         fs.writeFileSync(join(build, 'vi.json'), '{}', 'utf-8');
     });
 
-
     console.log(`- built ${postList.length} posts of ${folder}`)
-
-    // save to reduce fs.readFile() call for each browser
-    let freshBrowser = fs.readFileSync(
-        join('build', folder, 'index.html'),
-        'utf-8'
-    );
-
-    // generate the navigation then save back to the browser
-    {
-        freshBrowser = new JSDOM(freshBrowser);
-        freshBrowser = freshBrowser.window.document;
-        // get element as template
-        let tempE = freshBrowser.querySelector('.browser-nav').children[0];
-        freshBrowser.querySelector('.browser-nav').innerHTML = '';
-        for (let l1 = 0; l1 * postPerBrowser <= postList.length; l1++) {
-            // plus 1 to account for 1-index
-            tempE.setAttribute('href', `../${l1 + 1}/`);
-            tempE.innerHTML = l1 + 1;
-            freshBrowser.querySelector('.browser-nav').innerHTML += tempE.outerHTML;
-        }
-        // save pure text to prevent reference to object
-        freshBrowser = freshBrowser.documentElement.outerHTML;
-    }
 
     // generate browser pages
     let parentPage = ''; // the content of {folder}/index.html
@@ -138,33 +117,27 @@ function buildPosts(folder, config) {
     // because splice() is in place, only need to check the length
     // 1 index because the user is not a programmer
     for (let index = 1; postList.length > 0; index++) {
-        let browser = new JSDOM(freshBrowser);
-        browser = browser.window.document;
-
-        // add the list of posts
-        browser.querySelector('.browser-list').innerHTML +=
-            postList.splice(0, postPerBrowser).join('');
-
-        // highlight navigation
-        browser.querySelector('.browser-nav')
-        .children[index - 1].classList.add('current'); // -1 because 0-index children
-
         // copy first page
-        if (!parentPage) parentPage = browser.documentElement.outerHTML;
+        // if (!parentPage) parentPage = browser();
 
-        // write browser file
+        // write data file
         let browserPath = join(folder, index.toString());
+
         fs.mkdirSync(join('build', browserPath));
         fs.writeFileSync(
-            join('build', browserPath, 'index.html'),
-            browser.documentElement.outerHTML,
+            join('build', browserPath, 'vi.json'),
+            JSON.stringify({
+                // TODO: Spread translation from the main folder
+                postList: postList.splice(0, postPerBrowser),
+                browserPage: index - 1
+            }),
             'utf-8'
         )
 
-        // copy translation from parent folder
+        // copy browser file from parent folder
         fs.copyFileSync(
-            join('build', folder, 'vi.json'),
-            join('build', browserPath, 'vi.json')
+            join('build', folder, 'index.pug'),
+            join('build', browserPath, 'index.pug')
         )
 
         // add to release
@@ -172,15 +145,9 @@ function buildPosts(folder, config) {
     }
 
     // write the parent main page as the first page
-    parentPage = new JSDOM(parentPage);
-    parentPage = parentPage.window.document;
-    [...parentPage.querySelectorAll('.browser-nav > *')].forEach(e => {
-        e.setAttribute('href', e.innerHTML); // reset relative link
-    })
-    fs.writeFileSync(
-        join('build', folder, 'index.html'),
-        parentPage.documentElement.outerHTML,
-        'utf-8'
+    fs.copyFileSync(
+        join('build', folder, '1', 'vi.json'),
+        join('build', folder, 'vi.json')
     )
 
     // remove original post folder from build
