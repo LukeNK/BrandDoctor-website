61f937c8c4ceed0fa58c58ccf637f70e2403cb02
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-05-09 07:15:35 +0700

    Preparing for migration THE POINT OF NO RETURN

---
+++ dev/migrate.js
@@ -0,0 +1,33 @@
+const fs = require('fs'),
+    { JSDOM } = require('jsdom');
+
+for (let post of fs.readdirSync('tin-tuc/posts')) {
+    if (post.startsWith('!')) continue;
+
+    post = 'tin-tuc/posts/' + post;
+
+    // read data
+    let data = new JSDOM(fs.readFileSync(post, 'utf-8'));
+    data = data.window.document;
+
+    data = {
+        title: data.querySelector('a h2').innerHTML,
+        thumbnail: data.querySelector('a img').getAttribute('src'),
+        category: data.querySelector('a h4').innerHTML,
+        date: data.querySelector('a p').innerHTML,
+        content: data.querySelector('article').innerHTML,
+    }
+
+    fs.rmSync(post);
+
+    // fix file name
+    post = post.split('.');
+    post[post.length - 1] = 'json';
+    post = post.join('.');
+
+    fs.writeFileSync(
+        post,
+        JSON.stringify(data, undefined, '    '),
+        'utf-8'
+    )
+}
