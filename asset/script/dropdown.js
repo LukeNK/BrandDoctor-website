d96cbb93664f245cc517f143d882e8252a66a154
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-05-27 15:35:55 +0700

    `dropdown-click` completed

--- asset/script/dropdown.js
+++ asset/script/dropdown.js
@@ -2,11 +2,22 @@
 document.querySelectorAll('.dropdown-click')
 .forEach(dropdown => {
     dropdown.querySelectorAll('summary')
-    .forEach(sum =>
+    .forEach(sum => {
         sum.onclick = () =>
             dropdown.querySelectorAll('details')
-            .forEach(det =>
-                det.removeAttribute('open')
-            )
-    )
+                .forEach(elm =>elm.removeAttribute('open'))
+
+        // add button for desktop
+        let button = document.createElement('button');
+        button.innerHTML = sum.innerHTML;
+
+        button.onclick = () => {
+            dropdown.querySelectorAll('button')
+                .forEach(btn => btn.removeAttribute('open'));
+            button.setAttribute('open', 'true');
+            sum.click();
+        }
+
+        dropdown.querySelector('.desktop').append(button);
+    })
 })
