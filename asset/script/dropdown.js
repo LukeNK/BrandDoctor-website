6ec35b8203a6c1bfa01240fa64314a0ec7143da0
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-05-27 14:08:14 +0700

    Adjust indentation

--- asset/script/dropdown.js
+++ asset/script/dropdown.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 for(const dropdown of [...document.querySelectorAll('.dropdown-click')]) {
-        dropdown.onclick = () => {
-            [...dropdown.querySelectorAll('details')].forEach(e => {
-                e.removeAttribute('open')
-            })
-        }
+    dropdown.onclick = () => {
+        [...dropdown.querySelectorAll('details')].forEach(e => {
+            e.removeAttribute('open')
+        })
     }
+}
 })
