bfc62f84b96489ca2a4cda22e8b634cebecce40a
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-05-30 12:38:42 +0700

    Nav bar for mobile The language selector

--- asset/script/nav.js
+++ asset/script/nav.js
@@ -10,46 +10,10 @@
         }
 }
 
-// handle language selection
-const langToFlag = {
-    // list of flag representing each language
-    vi: 'vn',
-    en: 'gb',
-    fr: 'fr'
-}
-
-// handle the button to open pop-up menu
-// first element is the button to pop-up selection
-let langBtn = document.querySelector('#nav-lang *');
-
-// open pop-up
-langBtn.onclick = () => {
-    langBtn.classList.toggle('open')
-    langList.style.display = langList.style.display? '' : 'none';
-}
-
-// handle icon in the pop-up menu
-let langList = document.querySelector('#nav-lang ul');
-[...langList.children].forEach(elm => {
-    let flag = langToFlag[elm.getAttribute('lang')];
-    if (!flag) return; // skip no language
-    // add to the anchor itself
-    elm.firstChild.innerHTML =
-        `<span class="fi fi-${flag}"></span>`
-        + elm.innerHTML
-});
-
 // handle mobile
-{
+function toggleNav() {
     let nav = document.querySelector('nav'),
         navMenu = document.getElementById('nav-menu-close');
-    document.getElementById('nav-menu').onclick = () => {
-        nav.toggleAttribute('open');
-        navMenu.toggleAttribute('open');
-    };
-
-    navMenu.onclick = () => {
-        nav.removeAttribute('open');
-        navMenu.removeAttribute('open')
-    }
+    nav.toggleAttribute('open');
+    navMenu.toggleAttribute('open');
 }
