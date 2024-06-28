50cc4b8df48f87a02e561268b3a290c6d4e92da6
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-06-28 16:41:57 +0700

    Working bone

--- asset/script/timeline.js
+++ asset/script/timeline.js
@@ -2,7 +2,7 @@
 document.querySelectorAll('.time-prompt').forEach(timePrompt => {
     // create arrows for each prompt
     let arrows = document.createElement('div');
-    arrows.classList.add('time-arrow')
+    arrows.classList.add('time-arrow');
 
     for (let l1 = 0; l1 < timePrompt.childElementCount; l1++) {
         let arrow = document.createElement('div');
@@ -12,6 +12,21 @@ document.querySelectorAll('.time-prompt').forEach(timePrompt => {
 
     timePrompt.parentNode.insertBefore(arrows, timePrompt);
 
-    // asign close functions
 });
 
+// Assign each timeline to a time-prompt
+{
+    let timeline = document.querySelector('.timeline'),
+        timeArrow = document.querySelector('.time-arrow'),
+        timePrompt = document.querySelector('.time-prompt');
+
+    timeline.querySelectorAll('div').forEach(button => {
+        let index = Array.prototype.indexOf.call(timeline.children, button);
+        button.addEventListener('click', () => {
+            [...timePrompt.children].forEach(e => e.classList.remove('open'));
+            timePrompt.children[index].classList.add('open');
+
+            [...timeArrow.children].forEach(e => e.classList.remove('open'));
+            timeArrow.children[index].classList.add('open');        })
+    })
+}
